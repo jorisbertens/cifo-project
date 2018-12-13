cd3f51450abb437e499778177c5fba100f501cb8
Alexandra <alexandra_baur94@web.de>
2018-12-13 17:28:36 +0000

    Comments

--- project/baseline/utils/diversity.py
+++ project/baseline/utils/diversity.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-
 def phenotypic_entropy(population):
+    '''
+    measures the phenotypic entropy within the population (Vanneschi, 2018)
+    '''
     length = len(population)
     fitnesses = ["{:.4f}".format(ind.fitness) for ind in population]
     unique_fitnesses, counts = np.unique(fitnesses, return_counts=True)
@@ -9,11 +11,17 @@ def phenotypic_entropy(population):
 
 
 def phenotypic_variance(population):
+    '''
+    measures the phenotypic variance within the population (Vanneschi, 2018)
+    '''
     average = np.mean([ind.fitness for ind in population])
     return np.sum([ np.power((indiv.fitness - average) , 2) for indiv in population]) / (len(population) - 1)
 
 
 def genotypic_entropy(population):
+    '''
+    measures the genotypic entropy within a population (Vanneschi, 2018)
+    '''
     length = len(population)
     individuals = [["{:.4f}".format(neuron) for neuron in indiv.representation ] for indiv in population]
     unique_fitnesses, counts = np.unique(individuals, return_counts=True)
@@ -21,6 +29,9 @@ def genotypic_entropy(population):
 
 
 def genotypic_variance(population):
+    '''
+    measures the genotypic variance within a population (Vanneschi, 2018)
+    '''
     avg_distance = np.sum(np.abs([ind.representation for ind in population]))
     return np.sum( [np.power(np.sum(np.abs((indiv.representation - avg_distance))),2) for indiv in population]) / (len(population) - 1)
 
